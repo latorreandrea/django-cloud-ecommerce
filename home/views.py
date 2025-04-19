3cba262be210a6a58b4ce7aeeb770a9809f2899e
Andrea Latorre <latorre.andrea.93@gmail.com>
2025-04-19 10:50:53 +0200

    styled navbar and created logo bt

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    """View function for the home page."""
-    return render(request, 'home/index.html')
+    """ A view to render homepage """
+    return render(request, "home/index.html")
