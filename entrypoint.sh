e7275544d898771c3dcf226f632c1c1442d27ced
Andrea Latorre <latorre.andrea.93@gmail.com>
2025-05-22 12:50:48 +0200

    feat: Enable migrations in entrypoint script and remove temporary debug setting for Cloud Run

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,8 @@
 export SECRET_KEY=$(gcloud secrets versions access latest --secret=SECRET_KEY)
 export DATABASE_URL=$(gcloud secrets versions access latest --secret=DATABASE_URL)
 
+# execute migrations
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 exec "$@"
