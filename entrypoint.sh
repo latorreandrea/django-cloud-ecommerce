089145796521b3891b4deacdabf0a0675294b4f3
Andrea Latorre <latorre.andrea.93@gmail.com>
2025-06-17 08:07:41 +0200

    make always migrations in production

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,15 @@ export SECRET_KEY=$(gcloud secrets versions access latest --secret=SECRET_KEY)
 export DATABASE_URL=$(gcloud secrets versions access latest --secret=DATABASE_URL)
 
 
+# Waiting for database to be available (optional but recommended)
+echo "Waiting for database..."
+sleep 5
+
+# Run migrate in production
+echo "Applying database migrations..."
+python manage.py migrate
+
+# Start the application
+echo "Starting application..."
+
 exec "$@"
